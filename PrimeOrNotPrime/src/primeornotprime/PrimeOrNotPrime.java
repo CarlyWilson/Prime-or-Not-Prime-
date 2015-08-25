47fc947c8cf915a59a407456c4ae72009f95280b
Carly Wilson <Carly.Jo.Wilson2011@gmail.com>
2015-08-25 15:11:30 -0500

    Added code for determining if a number is prime or not prime efficiently.  added in a timer for timed, added in skeleton work for outcomes

--- PrimeOrNotPrime/src/primeornotprime/PrimeOrNotPrime.java
+++ PrimeOrNotPrime/src/primeornotprime/PrimeOrNotPrime.java
@@ -10,10 +10,13 @@ package primeornotprime;
 
 import java.util.Random;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class PrimeOrNotPrime {
 
     // Declare constants and variables here
+    boolean isPrime = false;
+    
     Random rand = new Random();
     Timer timer = new Timer();
     
@@ -72,43 +75,247 @@ public class PrimeOrNotPrime {
     }
     
     // Display Randomly Generated Number screen for classic
-    public void classicNumberGenerator(String difficultySetting){
+    public boolean classicNumberGenerator(String difficultySetting){
         switch(difficultySetting){
             case "Easy":
                 int randNumEasy = rand.nextInt((100 - 0) + 1) + 0;
+                isPrime = primenessChecker(randNumEasy);
+                switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                }
                 break;
             case "Medium":
                 int randNumMedium = rand.nextInt((1000 - 100) + 1) + 100;
+                isPrime = primenessChecker(randNumMedium);
+                switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                }
                 break;
             case "Hard":
                 int randNumHard = rand.nextInt((10000 - 1000) + 1) + 1000;
+                isPrime = primenessChecker(randNumHard);
+                switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                }
                 break;
             case "Insane":
                 int randNumInsane = rand.nextInt((100000 - 10000) + 1) + 10000;
+                isPrime = primenessChecker(randNumInsane);
+                switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // streak resets and wrong message is displayed
+                        }
+                }
                 break;
         }
     }
     
     //Display Randomly Generated Number screen for timed
-    public void timedNumberGenerator(String difficultySetting){
+    public boolean timedNumberGenerator(String difficultySetting){
         switch(difficultySetting){
                 case "Easy":
                     int randNumEasy = rand.nextInt((100 - 0) + 1) + 0;
-                    //timer
+                    isPrime = primenessChecker(randNumEasy);
+                    timer.schedule(new TimerTask(){
+                        public void run(){
+                            // Display game over screen with streaks
+                        }
+                    }, 2*60*1000);
+                    switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message timer cont
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                }
                     break;
                 case "Medium":
                     int randNumMedium = rand.nextInt((1000 - 100) + 1) + 100;
-                    //timer
+                    isPrime = primenessChecker(randNumMedium);
+                    timer.schedule(new TimerTask(){
+                        public void run(){
+                            // Display game over screen with streaks
+                        }
+                    }, 3*60*1000);
+                    switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message timer cont
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                }
                     break;
                 case "Hard":
                     int randNumHard = rand.nextInt((10000 - 1000) + 1) + 1000;
-                    //timer
+                    isPrime = primenessChecker(randNumHard);
+                    timer.schedule(new TimerTask(){
+                        public void run(){
+                            // Display game over screen with streaks
+                        }
+                    }, 4*60*1000);
+                    switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message timer cont
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                }
                     break;
                 case "Insane":
                     int randNumInsane = rand.nextInt((100000 - 10000) + 1) + 10000;
-                    //timer
+                    isPrime = primenessChecker(randNumInsane);
+                    timer.schedule(new TimerTask(){
+                        public void run(){
+                            // Display game over screen with streaks
+                        }
+                    }, 6*60*1000);
+                    switch(userSelection){
+                    case "Yes":
+                        if(userSelection == isPrime){
+                            return true;
+                            // load to factors screen and correct message timer cont
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                    case "No":
+                        if(userSelection == isPrime){
+                            return true;
+                            // reload screen with new number correct message
+                        }
+                        else{
+                            return false;
+                            // game over, display streak count
+                        }
+                }
                     break;
         }
+    }
+    
+    //Method to calculate if a number is prime or not prime
+    boolean primenessChecker(int randomNumber){
+        // Is randomNumber a multiple of two?
+        if(randomNumber % 2 == 0){
+            return false;
+        }
+        // If not check odds
+        for(int i = 3; i*i < randomNumber; i += 2){
+            if(randomNumber % i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //Method to calculate the Factors of a number
+    public void factors(int randomNumber){
+        
     }
     
     //Score screen
